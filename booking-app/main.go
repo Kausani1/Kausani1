627d36d74e2b8558f02f81bf3e0ad5a123f55e07
Kausani1 <145078755+Kausani1@users.noreply.github.com>
2023-11-16 01:45:27 +0000

    *

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {	
    fmt.Println("Welcome to our confererence booking application")
    fmt.Println("Get your tickets at the gate")
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
+
