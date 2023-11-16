8baac78ae1fb90f3a1111dbc2579890a1ccf3043
Kausani1 <145078755+Kausani1@users.noreply.github.com>
2023-11-16 02:00:01 +0000

    test

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,9 +5,8 @@ import (
 	"math"
 )
 
-func main() {	
-   fmt.Println("Welcome to our confererence booking application")
-   fmt.Println("Get your tickets at the gate")
+func main() {
+	fmt.Println("Welcome to our confererence booking application")
+	fmt.Println("Get your tickets a,mn,mn,mn,mnt the gate")
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
-
